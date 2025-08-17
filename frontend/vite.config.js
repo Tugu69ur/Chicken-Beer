8ebaaa7f553c3ae84389d8ab9fe6b7a9b3ec1ed5
Tugu69ur <149983686+Tugu69ur@users.noreply.github.com>
2025-08-17 13:12:27 +0800

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: "esbuild", // safe and fast
-    sourcemap: false, // saves memory
+    minify: "esbuild",
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
-            // split every dependency into its own chunk
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
+            return id.toString().split("node_modules/")[1].split("/")[0];
+          }
+          // split large local files if needed
+          if (id.includes("src/components/BigModule")) {
+            return "big-module";
           }
         },
       },
