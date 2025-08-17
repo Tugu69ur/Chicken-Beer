39fc4d227800e1d79bc3590c19cd1e351ef97ad5
Tugu69ur <149983686+Tugu69ur@users.noreply.github.com>
2025-08-17 12:55:44 +0800

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: "esbuild", // use esbuild instead of terser
+    sourcemap: false, // saves memory
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // split big deps
+        },
+      },
+    },
+  },
+});
