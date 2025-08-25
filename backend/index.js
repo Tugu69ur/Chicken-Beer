e4f5337e5f2d4866e12ee2b537d1bc0f385fa5ce
Tugu69ur <149983686+Tugu69ur@users.noreply.github.com>
2025-08-25 13:03:08 +0800

    f

--- backend/index.js
+++ backend/index.js
@@ -48,10 +48,9 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("{any}", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
