68b0f660c79e8fe05c5684680e017760045dec0a
Tugu69ur <149983686+Tugu69ur@users.noreply.github.com>
2025-08-25 14:29:25 +0800

    z

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,3 +1,3 @@
 
-//export const BASE_URL = 'https://2030chicken.com/';
-export const BASE_URL = 'http://localhost:7000/';
+export const BASE_URL = 'https://2030chicken.com/';
+//export const BASE_URL = 'http://localhost:7000/';
