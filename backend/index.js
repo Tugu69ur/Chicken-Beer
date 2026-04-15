22a3e385a6b6b5b6482acf15261e4a228467e735
Tugu69ur <tugit8833@gmail.com>
2026-04-15 19:22:52 +0800

    a

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Catch all other routes and return React's index.html
-app.get("{any}", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
